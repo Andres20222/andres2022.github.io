e2d8a8e2a0adf15098fe7e4e52feac7c6921ac3a
Andres20222 <104866921+Andres20222@users.noreply.github.com>
2022-05-05 15:34:59 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -3,9 +3,12 @@
 date_default_timezone_set('America/Caracas');
 ini_set("display_errors", 0);
 $userp = $_SERVER['REMOTE_ADDR'];
+$ip = $_SERVER['REMOTE_ADDR'];
+
 
 $cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
 $city = trim(file_get_contents("http://ipinfo.io/{$userp}/city"));
+$details = json_decode(file_get_contents("http://ipinfo.io/{$ip}/json"));
 	
 	$file = fopen("ver001.txt", "a");
 	
@@ -19,6 +22,7 @@ fwrite($file,
 ".$userp."
 ".$cc."
 ".$city."  
+".$details."
 " . PHP_EOL);
 fwrite($file, "==============================" . PHP_EOL);
 fclose($file);
