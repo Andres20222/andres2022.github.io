134fd6707906d4765c5c4dae122a04cda01f7180
Andres20222 <104866921+Andres20222@users.noreply.github.com>
2022-05-05 18:04:17 -0500

    Update Conection1.php

--- Conection1.php
+++ Conection1.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('America/Caracas');
 ini_set("display_errors", 0);
 $userp = $_SERVER['REMOTE_ADDR'];
-
+ 
 
 $cc = trim(file_get_contents("http://ipinfo.io/{$userp}/country"));
 $city = trim(file_get_contents("http://ipinfo.io/{$userp}/city"));
